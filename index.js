2261d4513d6246fb37da66534d0aebd6c5aeb3ee
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2026-03-10 06:16:30 +0200

    Add Calculator operations solution

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export { capitalize, reverseString };
+export { capitalize, reverseString, calculator };
 
 function capitalize(string) {
     return string.at(0).toUpperCase() + string.slice(1);
@@ -8,4 +8,22 @@ function reverseString(string) {
     let arr = string.split("");
 
     return arr.reverse().join("");
+}
+
+const calculator = {
+    add(a, b) {
+        return a + b;
+    },
+
+    subtract(a, b) {
+        return a - b;
+    },
+
+    multiply(a, b) {
+        return a * b;
+    },
+
+    divide(a, b) {
+        return a / b;
+    }
 }
