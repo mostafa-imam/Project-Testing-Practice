7706c7876adc9db5edbcf0f670ce3ca3d9ba6742
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2026-03-10 06:00:28 +0200

    Add reverseString() solution

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-export { capitalize };
+export { capitalize, reverseString };
 
 function capitalize(string) {
     return string.at(0).toUpperCase() + string.slice(1);
+}
+
+function reverseString(string) {
+    let arr = string.split("");
+
+    return arr.reverse().join("");
 }
