fa47c86c648f6fc10b70a03c65a2eddaf765f29a
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2026-03-10 05:59:55 +0200

    Add reverseString() test

--- index.test.js
+++ index.test.js
@@ -1,6 +1,10 @@
-import { capitalize } from "./index.js";
+import { capitalize, reverseString } from "./index.js";
 import { expect, test } from '@jest/globals';
 
-test("capitalize first letter", () => {
+test("Capitalize first letter", () => {
     expect(capitalize("book")).toBe("Book");
 });
+
+test("Reverse string", () => {
+    expect(reverseString("string")).toBe("gnirts");
+})
